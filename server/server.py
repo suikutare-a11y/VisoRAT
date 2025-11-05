34f50b110f7bef43779075b62f5f3c5463825472
suikutare-a11y <suikutare@gmail.com>
2025-11-05 23:21:43 +0100

    Update server.py

--- server/server.py
+++ server/server.py
@@ -270,4 +270,6 @@ if __name__ == '__main__':
         print(f"  Check duplicate tokens: {security.get('check_duplicate_tokens', True)}")
         print("  Username validation: Via player head API")
     
-    app.run(port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
